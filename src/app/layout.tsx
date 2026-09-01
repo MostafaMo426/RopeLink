f80e6a451bd2b2101cbd11c71287fd9ef3f1ee90
Safy <safymo81@gmail.com>
2026-09-01 03:40:39 +0300

    chore: update root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ type Props = {
   children: ReactNode;
 };
 
-// Root layout passing children to locale-specific layout
 export default function RootLayout({ children }: Props) {
   return children;
 }
